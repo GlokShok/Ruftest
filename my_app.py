e6b0e118430d532683ce0d8f2315bbbb9e1a62bd
DefaultDari <110895420+DefaultDari@users.noreply.github.com>
2022-08-11 12:25:59 +0300

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -1,1 +1,19 @@
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+app = QApplication([])
+first_win = QWidget()
+first_win.setWindowTitle("Здоровье")
+first_win.resize(1200,600)
+first_win.move(500,200)
+label =  QLabel(txt_hello)
+label1 =  QLabel(txt_instruction)
+h_line  = QHBoxLayout()
+h_line.addWidget(label, alignment = Qt.AlignCenter)
+h_line.addWidget(label1, alignment = Qt.AlignCenter)
+my_win.setLayout(h_line)
+my_win.show()
+app.exec_()
