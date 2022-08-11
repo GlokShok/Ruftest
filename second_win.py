46ffee1219e487930516dedf8b15998473fd0765
GlokShok <110895203+GlokShok@users.noreply.github.com>
2022-08-11 12:31:30 +0300

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,1 +1,22 @@
 # напиши здесь код для второго экрана приложения
+class TestWin(Qwidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_heigth)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.h_line = QHBoxLayout
+        self.r_line = QVBoxLayout
+        self.l_line = QVBoxLayout
+
+        
+    def connects(self):
+        pass
+    
